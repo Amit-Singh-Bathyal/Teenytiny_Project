4575431ab05ca910108311a88a3bcd76e50451d4
ashutosh <ashutoshsadana10@gmail.com>
2024-07-09 13:19:27 +0530

    vote poll routes-post

--- Backend/index.js
+++ Backend/index.js
@@ -3,7 +3,7 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 3000;
 const { connectMongoDB } = require("./connect");
-const Poll = require('./models/Poll');
+const Poll = require('./models/pollschema');
 
 connectMongoDB(process.env.MONGODB ?? "mongodb://localhost:27017/teenytiny").then(() =>
     console.log("Mongodb connected")
@@ -24,28 +24,27 @@ app.post('/createpolls', async (req, res) => {
     }
 });
 
-app.post('/vote',async()=>{
-  const{ pollId,option}=req.body;
- try {
-  const poll=await Poll.findById(pollId);
-  if (!poll) {return res.status(404).json({message:"poll not find"}); }
-if (!poll.options.includes(option)){
-  return res.status(400).json({message:'invalid option'});}
-  if(poll.votes.has(option)){
-    poll.votes.seet(option,poll.votes.get(option) +1);
-
-  }else{
-poll.votes.set(option,1);
-}
-  await poll.save();
-
-  res.status(200).json({messsage:'vote recorded',poll});
- } 
- catch (error) {
-  console.error('Error recording vote :',error);
-  res.status(500).json({message:'failed to record vote'});
-   
- } 
+app.post('/vote', async (req, res) => {
+  const { pollId, option } = req.body;
+  try {
+    const poll = await Poll.findById(pollId);
+    if (!poll) {
+      return res.status(404).json({ message: "poll not found" });
+    }
+    if (!poll.options.includes(option)) {
+      return res.status(400).json({ message: 'invalid option' });
+    }
+    if (poll.votes.has(option)) {
+      poll.votes.set(option, poll.votes.get(option) + 1);
+    } else {
+      poll.votes.set(option, 1);
+    }
+    await poll.save();
+    res.status(200).json({ message: 'vote recorded', poll });
+  } catch (error) {
+    console.error('Error recording vote:', error);
+    res.status(500).json({ message: 'failed to record vote' });
+  }
 });
 
 app.listen(port, () => {
