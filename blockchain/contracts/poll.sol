9f51c74cb2d3703c79c68fbdc012e762c200344f
ashutosh <ashutoshsadana10@gmail.com>
2024-07-16 19:10:40 +0530

    made vote function

--- blockchain/contracts/poll.sol
+++ blockchain/contracts/poll.sol
@@ -2,19 +2,35 @@ pragma solidity ^0.8.0;
 
    
 contract Poll {
-      struct polldata {
+    struct polldata {
         string question;
         string[] options;
-        uint256 pollid
+        uint256 pollid;
+        uint256[] votes;
     }
-   polldata[] public polls;
+    polldata[] public polls;
+    uint256 newpollid;
+
+
+  mapping(uint256 => mapping(uint256 => uint256)) public voteCounts;
+
+
+
 
     function createPoll(string memory QUESTION, string[] memory OPTIONS) public {
-        PollData storage newpoll = polls.push();
+        pollid=newpollid;
+        newpollid++;
+
+         polldata storage newpoll = polls.push();
         newpoll.question = QUESTION;
         newpoll.options = OPTIONS;
+        newpoll.pollid=newpollid
     }
-    function vote(uint pollid)
+   
+    function vote(uint256 pollId, uint256 optionIndex) public {
+    
+        voteCounts[pollId][optionIndex]++;
+    }
 
 
-} 
+}
