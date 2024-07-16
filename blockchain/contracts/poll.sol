03e0e7065d2a615ed3c059c84ecde2d2bf43281f
ashutosh <ashutoshsadana10@gmail.com>
2024-07-16 11:42:20 +0530

    Remove blockchain part folder

--- blockchain/contracts/poll.sol
+++ blockchain/contracts/poll.sol
@@ -2,17 +2,19 @@ pragma solidity ^0.8.0;
 
    
 contract Poll {
-      struct PollData {
+      struct polldata {
         string question;
         string[] options;
+        uint256 pollid
     }
-   PollData[] public polls;
+   polldata[] public polls;
 
     function createPoll(string memory QUESTION, string[] memory OPTIONS) public {
-        PollData storage newPoll = polls.push();
-        newPoll.question = QUESTION;
-        newPoll.options = OPTIONS;
+        PollData storage newpoll = polls.push();
+        newpoll.question = QUESTION;
+        newpoll.options = OPTIONS;
     }
+    function vote(uint pollid)
 
 
-}
+} 
